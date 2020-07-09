988133367429e2d16eb7c2c87d21f6dba96df745
sparker09 <sparker@student.wethinkcode.co.za>
2020-07-09 17:07:10 +0200

    added'

--- ex02/ft_print_numbers.c
+++ ex02/ft_print_numbers.c
@@ -1,8 +1,4 @@
-#include <unistd.h>
 
-void ft_putchar(char c)
-{ write(1, &c, 1);
-}
 void ft_print_numbers(void)
 {
     char number = '0';
--- ex03/ft_is_negative.c
+++ ex03/ft_is_negative.c
@@ -1,9 +1,4 @@
-#include <unistd.h>
 
-void ft_putchar(char c)
-{
-    write(1, &c, 1);
-}
 void ft_is_negative(int n)
 {
     if (n < 0)
