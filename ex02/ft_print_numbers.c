abe75fdfcc315be9f6c9462ab618f9113a16c364
sparker09 <sparker@student.wethinkcode.co.za>
2020-07-09 16:55:13 +0200

    second add

---
+++ ex02/ft_print_numbers.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{ write(1, &c, 1);
+}
+void ft_print_numbers(void)
+{
+    char number = '0';
+    while (number <= '9')
+{
+    ft_putchar(number);
+    number++;
+}
+
+}
+
